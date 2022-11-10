c853df70da7bff21c437541bf3e94609e82af5e6
Marwan-Mahmoud <108197024+Marwan-Mahmoud@users.noreply.github.com>
2022-11-10 04:51:45 +0200

    Update MyPanel.java

--- Connect 4/src/gui/MyPanel.java
+++ Connect 4/src/gui/MyPanel.java
@@ -2,7 +2,6 @@ package gui;
 
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,9 +13,9 @@ import connect4.IConnect4;
 
 public class MyPanel extends JPanel {
 
+	private IConnect4 connect4;
 	private Image background;
 	private List<Chip> chips;
-	private IConnect4 connect4;
 
 	public MyPanel(IConnect4 connect4) {
 		this.connect4 = connect4;
@@ -28,16 +27,16 @@ public class MyPanel extends JPanel {
 
 	@Override
 	public void paint(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.drawImage(background, 0, 0, null);
+		g.drawImage(background, 0, 0, null);
 		for (Chip chip : chips)
-			g2d.drawImage(chip.getColor(), 76 + 92 * chip.getX(), 35 + 92 * chip.getY(), 75, 75, null);
+			g.drawImage(chip.getColor(), 76 + 92 * chip.getX(), 35 + 92 * chip.getY(), 75, 75, null);
 	}
 
 	public void putChip(int column) {
 		Chip chip = connect4.put(column);
-		if (chip != null)
+		if (chip != null) {
 			chips.add(chip);
-		this.repaint();
+			this.repaint();
+		}
 	}
 }
